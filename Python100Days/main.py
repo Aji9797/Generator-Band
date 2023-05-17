70c087a4b9bbb1556681c187c565d5bd0e6dedd2
agum04 <agumsobarudin04@gmail.com>
2023-05-17 22:43:12 +0700

    first commit

---
+++ Python100Days/main.py
@@ -0,0 +1,4 @@
+print("Selamat Datang Di Penamaan Band")
+kota = input("Nama Kota Kamu Dibesarkan?\n")
+Peliharaan = input("Nama Peliharaan Kamu?\n ")
+print("Nama Band Kamu adalah \n" + kota + " " + Peliharaan)
